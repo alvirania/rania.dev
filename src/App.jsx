bae1b0ece3633ca5c54d62d46091bf940e3d8c64
Rania Alvi <rania.alvi@gmail.com>
2025-07-28 19:42:47 -0400

    theming v1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <main>
+      <h1 className= "text-5xl text-brown-100 font-bold ">brewbase</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      }
+      colors: {
+        brown: {
+          100: '#4F261A'
+        },
+        blue: {
+          50: '#f0f9ff',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
